48cf2ae7f0c3eac38ddf3a081bd2ee3c48056d88
kangmin <kangmin789@naver.com>
2019-02-10 13:45:03 +0900

    Updates

---
+++ precache-manifest.ec93cb4c73c6e29453e52c2978e60b88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kangmin46/ExReact_Movie.git/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "72600114abf03d2ca0ef",
+    "url": "/kangmin46/ExReact_Movie.git/static/js/runtime~main.72600114.js"
+  },
+  {
+    "revision": "bdf6b10ddbb000994477",
+    "url": "/kangmin46/ExReact_Movie.git/static/js/main.bdf6b10d.chunk.js"
+  },
+  {
+    "revision": "887714af23ad8d028ca1",
+    "url": "/kangmin46/ExReact_Movie.git/static/js/1.887714af.chunk.js"
+  },
+  {
+    "revision": "bdf6b10ddbb000994477",
+    "url": "/kangmin46/ExReact_Movie.git/static/css/main.988f0559.chunk.css"
+  },
+  {
+    "revision": "c0ede13ab2008fe12c124eba78059bcb",
+    "url": "/kangmin46/ExReact_Movie.git/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/kangmin46/ExReact_Movie.git/precache-manifest.ec93cb4c73c6e29453e52c2978e60b88.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/kangmin46/ExReact_Movie.git/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
